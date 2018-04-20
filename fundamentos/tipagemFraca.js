8b5e0a0847de244f9357f2c6564a79e300d384a0
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-04-20 17:34:17 -0300

    Tipagem Fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'Legal'; //Tipó texto
+console.log(qualquer);
+console.log(typeof qualquer); //Imprime o tipo da variável legal
+
+qualquer  = 3.212;
+console.log(typeof qualquer);
+
+//Evitar nomes genéricos e siglas.
+//Exemplo
+
+let valor = '';
+let numero = 1;
+let pqp = false; //Produto Quimico Perigoso 
