ec2c5e97dd5fd673f88d839c2af14d38f8adb1d6
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-04-20 18:05:18 -0300

    Alguns Cuidados com Javascript

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0); //Retorna infinito
+console.log("10" / 2); //Retorna 5
+console.log("Show" * 2); //Retorna Not a Number.
+console.log(0.1 + 0.7); //Retorna 7.9999
+//console.log(10.toString) //Da erro, no jeito literal não da certo
+console.log(7 / 0.01);
