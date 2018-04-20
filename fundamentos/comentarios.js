656a1c8b763e705901107c37bd0d3f330bbe657f
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-04-20 12:21:55 -0300

    Aula de comentários

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,17 @@
+// Comentário de uma única linha.
+console.log('Linha 1');
+
+
+/* 
+Comnetário 
+de multiplas 
+linhas
+*/
+console.log('Linha 2');
+
+/*
+*Comentário de Multiplas.
+*teste
+
+    
+*/    
