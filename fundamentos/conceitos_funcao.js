aa2ff6bae4a9c4e131ac8d9a2668c6d77d3d9be2
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-05-06 14:14:35 -0300

    Quase tudo é função

---
+++ fundamentos/conceitos_funcao.js
@@ -0,0 +1,11 @@
+//Se Javascript fosse um país, a Função seria o presidente.
+//A função exerce a função principal na linguagem.
+//Javascript é altamente centrado em Funções.
+//Exemplo
+
+
+console.log(typeof Object); //O objeto é uma função.
+
+class Produto {}
+
+console.log(typeof Produto);
