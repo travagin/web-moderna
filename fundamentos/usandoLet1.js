b298e58c43289bfec661fcdfc17f0cba9a95d75d
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-05-16 19:14:36 -0300

    Trabalahndo com Let 1

---
+++ fundamentos/usandoLet1.js
@@ -0,0 +1,16 @@
+var numero = 1;
+{
+    let numero = 2;
+    console.log('Número dentro = ' +numero)
+}
+
+console.log('Número fora = ' +numero);
+
+//Variaveis definidas com var
+//Tem escopo de função
+//Tem escopo global
+
+//Variaveis definidas com var
+//Tem escopo de função
+//Tem escopo global
+//Tem escopo de bloco.
