9d078750be23219bb57f918c8a69a084730f229c
Lucas Travagin <lucas_travagin1112@hotmail.com>
2018-05-30 12:09:46 -0300

    Trabalhando com objeto parte 2

---
+++ fundamentos/objeto2.js
@@ -0,0 +1,12 @@
+console.log(typeof Object); //Função
+
+console.log(typeof new Object()); // Objeto
+
+const Cliente = function() {}
+console.log(typeof Cliente); //Função
+console.log(typeof new Cliente); //Objeto
+
+class Produto {} // uma classe
+console.log(typeof Produto); //Função
+console.log(typeof new Produto()); //Objeto
+
