1448928bd39542367babe18848cd970ea9142726
Lucas <lucas_travagin1112@hotmail.com>
2018-08-09 09:58:54 -0300

    Trabalhando com o operadores aritméticos

---
+++ fundamentos/aritimeticos.js
@@ -0,0 +1,7 @@
+//Criando uma constante com o operador Destructuring
+const [a,b,c,d] = [3,5,1,15]
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicao = a * b
+const divisao = d/a
+const modulo = a % 2
