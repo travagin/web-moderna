6814954c24b575669c7dbf72b738f88963b1e374
Lucas <lucas_travagin1112@hotmail.com>
2018-08-19 11:20:56 -0300

    trabalhando com Else IF

---
+++ estruturasDeControle/ifElseIf.js
@@ -0,0 +1,24 @@
+Number.prototype.entre = function(inicio, fim) {
+    return this >= inicio && this <= fim
+}
+
+const imprimirResultado = function(nota) {
+    if(nota.entre(9,10)) {
+        console.log('Quadro de Honra')
+    } else if(nota.entre(7, 8.99)) {
+        console.log('Aprovaddo')
+    } else if(nota.entre(4, 6.99)) {
+        console.log('Recuperação')
+    } else if(nota.entre(0, 3.99)) {
+        console.log('Reprovado')
+    } else {
+        console.log('Nota inválida')
+    }
+
+    console.log('Fim')
+}
+
+console.log(10)
+console.log(8.9)
+console.log(6.55)
+console.log(-1)
