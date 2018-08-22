769ccacbc7f90b1e9b62ffa6dce8c79cfc215811
Lucas <lucas_travagin1112@hotmail.com>
2018-08-22 12:30:37 -0300

    Trabalahdo com For

---
+++ estruturasDeControle/for1.js
@@ -0,0 +1,14 @@
+let contador = 1
+while(contador <=10) {
+    console.log(`contador = ${contador}`) //Template String
+}
+
+for(let i = 0; i <=10; i++) {
+    console.log(`Contador ${i}`)
+}
+
+const notas = [6,7,8,9,10]
+for(i = 0; i <= notas.length; i++) {
+    console.log(`Notas = ${notas[i]}`)
+}
+
