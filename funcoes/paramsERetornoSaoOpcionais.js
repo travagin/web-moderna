b7a827ea62ce8c9fa74a864096696039632ac150
Lucas <lucas_travagin1112@hotmail.com>
2018-08-22 17:58:40 -0300

    Parâmetros e retornos são opcionais

---
+++ funcoes/paramsERetornoSaoOpcionais.js
@@ -0,0 +1,16 @@
+function area(largura,altura) {
+    const area = largura * altura
+    if(area > 20) {
+        console.log(`Valor acima do permitido ${area} m2.`)
+    } else {
+        return area
+    }
+}
+
+console.log(area(2,2)) //4
+console.log(area(2)) //NaN
+console.log(area()) //NaN
+Console.LOG(2,3,17,3) //6
+
+
+
