85e637c2ce2aac6b96fff2dffe75d4a9fceaf1db
Lucas <lucas_travagin1112@hotmail.com>
2018-08-24 12:07:47 -0300

    Trabalhando com Arrow Functions 3

---
+++ funcoes/arrowFunction3.js
@@ -0,0 +1,20 @@
+let comparaComThis = function(params) {
+    console.log(this === params)
+}
+
+comparaComThis(global)
+const obj = {
+}
+
+comparaComThis = comparaComThis.bind(obj)
+
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
