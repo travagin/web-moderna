444272e74a6aad5473aa38d22f0f3ecf0c02b5ef
Lucas <lucas_travagin1112@hotmail.com>
2018-08-26 14:54:51 -0300

    Introdução a Orientção a Objetos com Javascript

---
+++ objetos/oo.js
@@ -0,0 +1,21 @@
+//Código Não executável
+
+// Procedural
+processamento(valor1,valor2,valor3) //O foco neste contexto é a função.
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {}
+}
+
+objeto.processamento() //Foco passou a ser o objeto
+
+// Príncipios importantes
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (prototype)
+// 4. Polimorfismo
+
