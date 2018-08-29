f0f99aeffda77ec42a94da94f2416730576164d9
Lucas <lucas_travagin1112@hotmail.com>
2018-08-29 15:04:10 -0300

    Carregando arquivos de Imagens

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,24 @@
+const modoDev = process.env.NODE_ENV !== 'producion'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UnglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UnglifyJsPlugin( {
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
