dc8d349d6acc46e9a3e2466ee3720f9f666a0bc3
Lucas <lucas_travagin1112@hotmail.com>
2018-08-29 15:29:44 -0300

    Objetos Constantes

--- objetos/objetosConstantes.js
+++ objetos/objetosConstantes.js
@@ -1,1 +1,13 @@
+//Oque não muda é o endereço de memória.
+
+//Da certo
 const pessoa = {nome: 'João'}
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+//Pessoa está recebendo um novo objeto
+pessoa = {nome: 'Ana'}
+
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
