206712ce19609c1d2538e9041c7ec2e0d8abae03
Lucas <lucas_travagin1112@hotmail.com>
2018-08-29 12:28:35 -0300

    Carregando arquivos .css

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,5 +1,5 @@
-const Pessoa = require('./pessoa')
-//import Pessoa from './pessoa'
+import Pessoa from './pessoa'
+import './assets/css/style.css'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
